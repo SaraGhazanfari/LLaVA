5de43297b46af5498d01923a0008a62a118f7788
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-07-25 09:49:37 -0400

    new pretrain script

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -1,4 +1,5 @@
 import os
+
 from .clip_encoder import CLIPVisionTower, CLIPVisionTowerS2, VaRVisionTower
 
 
@@ -6,7 +7,8 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
     vision_tower = getattr(vision_tower_cfg, 'mm_vision_tower', getattr(vision_tower_cfg, 'vision_tower', None))
     is_absolute_path_exists = os.path.exists(vision_tower)
     use_s2 = getattr(vision_tower_cfg, 's2', False)
-    if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion") or "ShareGPT4V" in vision_tower:
+    if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith(
+        'sara') or vision_tower.startswith("laion") or "ShareGPT4V" in vision_tower:
         if vision_tower.startswith('sara'):
             return VaRVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
         elif use_s2:
