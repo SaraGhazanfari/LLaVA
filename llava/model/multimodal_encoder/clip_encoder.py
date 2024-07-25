5668be87599ae80038c882281a8468b2397afa9b
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-07-25 10:54:13 -0400

    new pretrain script

--- llava/model/multimodal_encoder/clip_encoder.py
+++ llava/model/multimodal_encoder/clip_encoder.py
@@ -160,6 +160,28 @@ class VaRVisionTower(CLIPVisionTower):
 
         return image_features
 
+    @property
+    def config(self):
+        return CLIPVisionConfig({
+            "_name_or_path": "openai/clip-vit-large-patch14-336",
+            "attention_dropout": 0.0,
+            "dropout": 0.0,
+            "hidden_act": "quick_gelu",
+            "hidden_size": 1024,
+            "image_size": 336,
+            "initializer_factor": 1.0,
+            "initializer_range": 0.02,
+            "intermediate_size": 4096,
+            "layer_norm_eps": 1e-05,
+            "model_type": "clip_vision_model",
+            "num_attention_heads": 16,
+            "num_channels": 3,
+            "num_hidden_layers": 24,
+            "patch_size": 14,
+            "projection_dim": 768,
+            "transformers_version": "4.42.4"
+        })
+
 
 class CLIPVisionTowerS2(CLIPVisionTower):
     def __init__(self, vision_tower, args, delay_load=False):
