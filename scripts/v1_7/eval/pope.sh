bd60b975787f61ddee2ff0a463340b24ecb4e0ba
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-07-29 15:16:30 -0400

    update the gqa script

---
+++ scripts/v1_7/eval/pope.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+$SCRATCH/aa10460/pytorch-example/python -m llava.eval.model_vqa_loader \
+    --model-path liuhaotian/llava-v1.7-7b \
+    --question-file $VAST/eval/pope/llava_pope_test.jsonl \
+    --image-folder $VAST/eval/pope/val2014 \
+    --answers-file $VAST/eval/pope/answers/llava-v1.7-7b.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+$SCRATCH/aa10460/pytorch-example/python llava/eval/eval_pope.py \
+    --annotation-dir $VAST/eval/pope/coco \
+    --question-file $VAST/eval/pope/llava_pope_test.jsonl \
+    --result-file $VAST/eval/pope/answers/llava-v1.7-7b.jsonl
