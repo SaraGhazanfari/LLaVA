d8e94e7f1211ffc768c015b707b72a20bce07431
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-07-29 15:39:58 -0400

    update the gqa script

--- scripts/v1_7/eval/mmbench.sh
+++ scripts/v1_7/eval/mmbench.sh
@@ -3,9 +3,9 @@
 SPLIT="mmbench_dev_20230712"
 
 $SCRATCH/aa10460/pytorch-example/python -m llava.eval.model_vqa_mmbench \
-    --model-path ./checkpoints/llava-v1.75-7b \
+    --model-path ./checkpoints/llava-v1.7-7b \
     --question-file $VAST/eval/mmbench/$SPLIT.tsv \
-    --answers-file $VAST/eval/mmbench/answers/$SPLIT/llava-v1.75-7b.jsonl \
+    --answers-file $VAST/eval/mmbench/answers/$SPLIT/llava-v1.7-7b.jsonl \
     --single-pred-prompt \
     --temperature 0 \
     --conv-mode vicuna_v1
@@ -16,4 +16,4 @@ $SCRATCH/aa10460/pytorch-example/python scripts/convert_mmbench_for_submission.p
     --annotation-file $VAST/eval/mmbench/$SPLIT.tsv \
     --result-dir $VAST/eval/mmbench/answers/$SPLIT \
     --upload-dir $VAST/eval/mmbench/answers_upload/$SPLIT \
-    --experiment llava-v1.75-7b
+    --experiment llava-v1.7-7b
