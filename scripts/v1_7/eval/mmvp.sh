f95e908799c50ae8c14d84824039ad6c2f8c710e
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-07-29 17:19:19 -0400

    add mmvp

--- scripts/v1_7/eval/mmvp.sh
+++ scripts/v1_7/eval/mmvp.sh
@@ -1,1 +1,1 @@
-$SCRATCH/aa10460/pytorch-example/python llava/eval/eval_mmvp.py --directory $VAST/eval/vision-bench/mmvp --model-path ./checkpoints/llava-v1.7-7b
+$SCRATCH/aa10460/pytorch-example/python llava/eval/eval_mmvp.py --directory $VAST/eval/vision-bench/MMVP --model-path ./checkpoints/llava-v1.7-7b
