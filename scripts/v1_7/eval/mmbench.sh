952bc856f957cf6e51c898b6e98661c492c5215a
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-07-29 15:37:16 -0400

    update the gqa script

---
+++ scripts/v1_7/eval/mmbench.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SPLIT="mmbench_dev_20230712"
+
+$SCRATCH/aa10460/pytorch-example/python -m llava.eval.model_vqa_mmbench \
+    --model-path ./checkpoints/llava-v1.75-7b \
+    --question-file $VAST/eval/mmbench/$SPLIT.tsv \
+    --answers-file $VAST/eval/mmbench/answers/$SPLIT/llava-v1.75-7b.jsonl \
+    --single-pred-prompt \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+mkdir -p $VAST/eval/mmbench/answers_upload/$SPLIT
+
+$SCRATCH/aa10460/pytorch-example/python scripts/convert_mmbench_for_submission.py \
+    --annotation-file $VAST/eval/mmbench/$SPLIT.tsv \
+    --result-dir $VAST/eval/mmbench/answers/$SPLIT \
+    --upload-dir $VAST/eval/mmbench/answers_upload/$SPLIT \
+    --experiment llava-v1.75-7b
---
+++ scripts/v1_7/eval/mmbench_cn.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SPLIT="mmbench_dev_cn_20231003"
+
+$SCRATCH/aa10460/pytorch-example/python -m llava.eval.model_vqa_mmbench \
+    --model-path ./checkpoints/llava-v1.7-7b \
+    --question-file $VAST/eval/mmbench_cn/$SPLIT.tsv \
+    --answers-file $VAST/eval/mmbench_cn/answers/$SPLIT/llava-v1.7-7b.jsonl \
+    --lang cn \
+    --single-pred-prompt \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+mkdir -p $VAST/eval/mmbench/answers_upload/$SPLIT
+
+$SCRATCH/aa10460/pytorch-example/python scripts/convert_mmbench_for_submission.py \
+    --annotation-file $VAST/eval/mmbench_cn/$SPLIT.tsv \
+    --result-dir $VAST/eval/mmbench_cn/answers/$SPLIT \
+    --upload-dir $VAST/eval/mmbench_cn/answers_upload/$SPLIT \
+    --experiment llava-v1.7-7b
